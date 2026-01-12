8518ef02543b30c1ceddd380fd4d49dc9737b092
vcnt <thiago.vicente@ua.pt>
2026-01-12 13:11:09 +0000

    Remove lazy init of model

--- src/embeddings/model.py
+++ src/embeddings/model.py
@@ -1,4 +1,3 @@
-from functools import lru_cache
 from sentence_transformers import SentenceTransformer
 import os
 
@@ -7,18 +6,15 @@ ALLOWED_MODELS = {
     "all-mpnet-base-v2",
 }
 
-# load model from env
+# load model version from env
 MODEL: str = os.getenv("MODEL", "all-MiniLM-L6-v2")
 if MODEL not in ALLOWED_MODELS:
     raise ValueError(f"Model {MODEL} is not in the allowlist")
 
 device = os.getenv("DEVICE", "cpu")
 
-model: SentenceTransformer | None = None
+# Load model at module import time
+_model: SentenceTransformer = SentenceTransformer(MODEL, device=device)
 
-@lru_cache
 def get_model() -> SentenceTransformer:
-    global model
-    if model is None:
-        model = SentenceTransformer(MODEL, device=device)
-    return model
+    return _model
